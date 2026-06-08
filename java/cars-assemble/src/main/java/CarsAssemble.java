ec84f5c89de578f28a198d94f11a5a70cfc53697
kesav <kesav424@gmail.com>
2026-06-08 16:21:52 +0530

    cars assemble pass

--- java/cars-assemble/src/main/java/CarsAssemble.java
+++ java/cars-assemble/src/main/java/CarsAssemble.java
@@ -1,10 +1,22 @@
 public class CarsAssemble {
 
     public double productionRatePerHour(int speed) {
-        throw new UnsupportedOperationException("Please implement the CarsAssemble.productionRatePerHour() method");
+
+        double successRate = 0.0;
+        ;
+        if (speed >= 1 && speed <= 4) {
+            successRate = 1.0;
+        } else if (speed >= 5 && speed <= 8) {
+            successRate = 0.9;
+        } else if (speed == 9) {
+            successRate = 0.8;
+        } else if (speed == 10) {
+            successRate = 0.77;
+        }
+        return speed * 221 * successRate;
     }
 
     public int workingItemsPerMinute(int speed) {
-        throw new UnsupportedOperationException("Please implement the CarsAssemble.workingItemsPerMinute() method");
+        return (int) productionRatePerHour(speed) / 60;
     }
 }
